bdda782b2d86b631be39075caff06f9bfa56e7ef
Евгения Борисова <borisova.ev.2019@yandex.ru>
2021-06-01 08:21:46 +0300

    Правки после ревью

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
-function getRandomNumber (minNumber, maxNumber) {
-  if (maxNumber > minNumber) {
+const comment = document.querySelector('.social__footer-text');
+
+function getRandomNumber(minNumber, maxNumber) {
+  if (maxNumber > minNumber && maxNumber > 0 && minNumber >= 0) {
     minNumber = Math.ceil(minNumber);
     maxNumber = Math.floor(maxNumber);
     return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
   }
-  const error  = 'Введите другие числа';
+  const error = 'Введите другие числа';
   return error;
 }
-getRandomNumber ();
+getRandomNumber(1, 8);
 
-function checkStringLength (nameString, maxValue) {
-  return nameString.length < maxValue;
+function checkStringLength(nameString, maxValue) {
+  return nameString.length <= maxValue;
 }
-checkStringLength ();
+checkStringLength(comment, 140);
