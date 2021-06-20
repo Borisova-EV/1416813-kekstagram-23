0d051b21406e0095550a2a43bf1dc139fee3574f
Евгения Борисова <borisova.ev.2019@yandex.ru>
2021-06-20 18:40:05 +0300

    Добавление функции для создания галереи добавленных фото

---
+++ js/create-gallery-photos.js
@@ -0,0 +1,19 @@
+import { createPhotos } from './create-photos.js';
+// Блок для добавленных фото
+const PICTURES_CONTAINER = document.querySelector('.pictures');
+// Шаблон добавленных фото
+const PICTURE_TEMPLATE = document.querySelector('#picture').content.querySelector('.picture');
+const DESCRIPTION_PHOTOS = createPhotos();
+
+// Создание галереи добавленных фото
+function createGalleryPhotos () {
+  DESCRIPTION_PHOTOS.forEach((picture) => {
+    const newPicture = PICTURE_TEMPLATE.cloneNode(true);
+    newPicture.querySelector('.picture__img').src = picture.url;
+    newPicture.querySelector('.picture__likes').textContent = picture.likes;
+    newPicture.querySelector('.picture__comments').textContent = picture.comments.length;
+    PICTURES_CONTAINER.appendChild(newPicture);
+  });
+}
+
+export {createGalleryPhotos};
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 // Импорт модулей
-import { createPhotos } from './create-photos.js';
+import { createGalleryPhotos } from './create-gallery-photos.js';
 
-createPhotos();
+createGalleryPhotos();
