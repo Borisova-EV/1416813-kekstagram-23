73f0f7b209034be7c8b8500d8e1e0ad1f1350c03
Евгения Борисова <borisova.ev.2019@yandex.ru>
2021-07-18 19:53:21 +0300

    Устраняет конфликт в модуле form.js

---
+++ js/change-scale.js
@@ -0,0 +1,27 @@
+import { inputScaleValue } from './form.js';
+
+const photoPreviewContainer = document.querySelector('.img-upload__preview');
+const photoPreview = photoPreviewContainer.querySelector('img');
+
+let scalePercent = 100;
+
+const changeScalePhoto = () => {
+  const value = scalePercent / 100;
+  photoPreview.style.transform = `scale(${value})`;
+};
+
+const decreaseScale = () => {
+  if (scalePercent !== 25) {
+    scalePercent -= 25;
+  }
+};
+
+const increaseScale = () => {
+  if (scalePercent !== 100) {
+    scalePercent += 25;
+  }
+};
+
+const changeValueInputScale = () => inputScaleValue.value = `${scalePercent}%`;
+
+export { decreaseScale, changeValueInputScale, changeScalePhoto, increaseScale };
--- js/form.js
+++ js/form.js
@@ -1,6 +1,7 @@
 import { isEscEvent, isFocusElement } from './utils.js';
 import { validationHashTags } from './validation-hash-tags.js';
 import { validationComments } from './validation-comments.js';
+import { decreaseScale, changeValueInputScale, changeScalePhoto, increaseScale } from './change-scale.js';
 
 const uploadPictureForm = document.querySelector('.img-upload__form');
 const editPicturePopup = uploadPictureForm.querySelector('.img-upload__overlay');
@@ -8,17 +9,34 @@ const uploadPictureInput = uploadPictureForm.querySelector('#upload-file');
 const closeEditPictureButton = uploadPictureForm.querySelector('#upload-cancel');
 const hashtagsInput = uploadPictureForm.querySelector('.text__hashtags');
 const commentsInput = uploadPictureForm.querySelector('.text__description');
+const smallerScaleButton = uploadPictureForm.querySelector('.scale__control--smaller');
+const biggerScaleButton = uploadPictureForm.querySelector('.scale__control--bigger');
+const inputScaleValue = uploadPictureForm.querySelector('.scale__control--value');
 
 const onDocumentKeydown = (evt) => {
   if (isEscEvent(evt) && !(isFocusElement(hashtagsInput)) && !(isFocusElement(commentsInput))) {
     evt.preventDefault();
-    // eslint-disable-next-line no-use-before-define
     closeEditPopup();
   }
 };
 
-// eslint-disable-next-line no-use-before-define
 const onCloseEditPictureButtonClick = () => closeEditPopup();
+
+const onHashtagsInputChange = () => validationHashTags();
+
+const onCommentsInputInput = () => validationComments();
+
+const onSmallerScaleButtonClick = () => {
+  decreaseScale();
+  changeValueInputScale();
+  changeScalePhoto();
+};
+
+const onBiggerScaleButtonClick = () => {
+  increaseScale();
+  changeValueInputScale();
+  changeScalePhoto();
+};
 
 //Функция закрытия окна редактирования загружаемого фото
 function closeEditPopup() {
@@ -27,8 +45,17 @@ function closeEditPopup() {
   uploadPictureInput.value = '';
   hashtagsInput.value = '';
   commentsInput.value = '';
+
+  hashtagsInput.removeEventListener('change', onHashtagsInputChange);
+
+  commentsInput.removeEventListener('input', onCommentsInputInput);
+
+  smallerScaleButton.removeEventListener('click', onSmallerScaleButtonClick);
+  biggerScaleButton.removeEventListener('click', onBiggerScaleButtonClick);
+
   document.removeEventListener('keydown', onDocumentKeydown);
   closeEditPictureButton.removeEventListener('click', onCloseEditPictureButtonClick);
+
 }
 
 //Функция открытия окна редактирования загружаемого фото
@@ -36,12 +63,17 @@ const openEditPopup = () => {
   editPicturePopup.classList.remove('hidden');
   document.body.classList.add('modal-open');
 
-  hashtagsInput.addEventListener('change', () => validationHashTags());
+  hashtagsInput.addEventListener('change', onHashtagsInputChange);
 
-  commentsInput.addEventListener('input', () => validationComments());
+  commentsInput.addEventListener('input', onCommentsInputInput);
+
+  inputScaleValue.value = '100%';
+  smallerScaleButton.addEventListener('click', onSmallerScaleButtonClick);
+  biggerScaleButton.addEventListener('click', onBiggerScaleButtonClick);
 
   document.addEventListener('keydown', onDocumentKeydown);
   closeEditPictureButton.addEventListener('click', onCloseEditPictureButtonClick);
 };
 
-export { hashtagsInput, commentsInput,uploadPictureInput, openEditPopup};
+export { uploadPictureInput, hashtagsInput, commentsInput, inputScaleValue, openEditPopup };
+
